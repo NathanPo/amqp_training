7a5dacb29d48f034edee842c5dfa6b61d733b8d7
Nathan PODESTA <nathan.podesta@icloud.com>
2022-01-26 17:33:35 +0100

    remove second main

--- server/src/main/java/fr/lernejo/chat/ChatMessageController.java
+++ server/src/main/java/fr/lernejo/chat/ChatMessageController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/message")
 public class ChatMessageController {
 
     @GetMapping
-    public String getLastTenMessages() {
-        return ChatMessageRepository.getLastTenMessages().toString();
+    public List<String> getLastTenMessages() {
+        return ChatMessageRepository.getLastTenMessages();
     }
 }
