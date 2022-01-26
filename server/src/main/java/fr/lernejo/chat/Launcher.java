b609bed6105af0e7dc7fc9a67794d6d536103bfd
Nathan PODESTA <nathan.podesta@icloud.com>
2022-01-26 16:04:45 +0100

    define client ampq

---
+++ server/src/main/java/fr/lernejo/chat/Launcher.java
@@ -0,0 +1,10 @@
+package fr.lernejo.chat;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Launcher {
+    public static void main(String[] args) {
+        
+    }
+}
